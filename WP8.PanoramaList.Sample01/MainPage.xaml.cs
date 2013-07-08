629ae66ff58f15c551907c9e73a0bba0345a6fbc
Antonio Baker <abakerp@gmail.com>
2013-07-08 17:45:52 +0100

    initial commit
    initial commit. Basic arrangement of Panorama Controls triggered by top
    main navigation using the visual state manager.

---
+++ WP8.PanoramaList.Sample01/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WP8.PanoramaList.Sample01.Resources;
+
+namespace WP8.PanoramaList.Sample01
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void section1_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            VisualStateManager.GoToState(this,"One", false);
+        }
+
+        private void section2_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Two", true);
+        }
+
+        private void section3_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Three", true);
+        }
+
+        private void section4_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Four", true);
+        }
+    }
+}
